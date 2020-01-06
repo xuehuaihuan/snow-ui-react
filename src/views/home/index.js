6f01fe1742d0ee63a3aac5925987c14aaa0a6246
Huaihuan Xue <xuehuaihuan@163.com>
2020-01-06 11:27:07 +0800

    add home page - 添加首页；

---
+++ src/views/home/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import StyledHome from './styles';
+import { Flex } from '../../lib/flex';
+import { FlexItem } from '../../lib/flex-item';
+
+const list = [
+  {
+    title: 'Button（按钮）',
+    id: 'button',
+    path: '/button',
+  },
+  {
+    title: 'Spinner icon（旋转图标）',
+    id: 'spinner',
+    path: '/spinner',
+  },
+  {
+    title: 'Ripple（涟漪）',
+    id: 'ripple',
+    path: '/ripple',
+  },
+];
+
+class Home extends Component {
+  constructor () {
+    super();
+    this.list = list;
+  }
+
+  render () {
+    return (
+      <StyledHome>
+        {
+          this.list.map((item) => {
+            return (
+              <Flex
+                key={item.id}
+                onClick={() => this.handleClickItem(item)}
+              >
+                <FlexItem flex={1}>
+                  <h4>{item.title}</h4>
+                </FlexItem>
+                <FlexItem>
+                  <p>
+                    <i className='iconfont iconfont-arrow-right' />
+                  </p>
+                </FlexItem>
+              </Flex>
+            );
+          })
+        }
+      </StyledHome>
+    );
+  }
+
+  handleClickItem = (item) => {
+    this.props.history.push(item.path);
+  }
+};
+
+Home.defaultProps = {};
+
+Home.propTypes = {
+  history: PropTypes.object,
+};
+
+export default Home;
---
+++ src/views/home/styles/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledHome = styled.div`
+  background-color: ${props => props.theme.lightColor};
+`;
+
+export default StyledHome;
