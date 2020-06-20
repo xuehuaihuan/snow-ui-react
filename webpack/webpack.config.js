4128bb99613c41ae50f540f77be3c8c49ac9c081
Huaihuan Xue <xuehuaihuan@163.com>
2020-06-20 20:16:04 +0800

    webpack 调整；

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -15,18 +15,33 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(jsx|ts|tsx)$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        loader: 'ts-loader',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: ['@babel/plugin-proposal-class-properties'],
+            },
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
       },
       {
-        test: /\.(js|mjs)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react'],
-          plugins: ['@babel/plugin-proposal-class-properties'],
-        },
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: ['@babel/plugin-proposal-class-properties'],
+            },
+          },
+        ],
       },
     ],
   },
