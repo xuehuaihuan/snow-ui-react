4873ca5175431ec2d698294b8f0400597431e7bc
Huaihuan Xue <xuehuaihuan@163.com>
2020-06-25 15:39:39 +0800

    格式化.eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  "extends": [
+  extends: [
     'standard-react',
     'standard',
-    "plugin:@typescript-eslint/recommended",
+    'plugin:@typescript-eslint/recommended',
   ],
-  "parser": "@typescript-eslint/parser",
-  "plugins": ["@typescript-eslint"],
-  "rules": {
-    "semi": ["error", "always"],
-    "comma-dangle": ["error", "always-multiline"],
-    "@typescript-eslint/no-var-requires": 0
-  }
-}
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/no-var-requires': 0,
+  },
+};
